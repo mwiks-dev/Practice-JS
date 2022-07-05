dad15313e573f72736f7e22150458153b8a50afb
Maryann <mwikali119@gmail.com>
2022-07-05 23:58:14 +0300

    Went through array methods and debugging using built in debugger in node.js

---
+++ debugger.js
@@ -0,0 +1,9 @@
+//using the debugger built in to Node.js.
+// Place one or more breakpoints in your code using the debugger keyword
+// Run node inspect <filename> in the terminal to launch the debugger
+// Run cont at the debug prompt to start execution of the code and advance to the first breakpoint
+// Run repl at the debug prompt to enter the debugger's REPL
+// Check the value of any variables by entering the variable name at the > prompt
+// Exit the REPL using ctrl-C
+// Continue stepping through the code using cont
+// Run .exit or type ctrl-C twice to exit the debugger
---
+++ objects.js
@@ -0,0 +1,19 @@
+// Definition = Like Arrays, JavaScript Objects are collections of data. They consist of a list of properties (key-value pairs) bounded by curly braces ({ }). The properties can point to values of any data type — even other Objects.
+// Ways to access values in objects
+// 1. Bracket notation
+// 2. Dot notation
+// 3. Property Descriptor
+
+// Object Methods
+// Object.keys()
+// Object.values()
+// Object.entries()
+// Object.hasOwnProperty()
+
+//Modify an Object
+// add property to an object using dot or bracket Notation
+const person = {}
+person.name = "John";
+person.age = 30;
+console.log(person)
+//Undestructively add a property to an object using spread operator(Research to be done!!!)
